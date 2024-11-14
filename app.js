9e8dc00fee46efe07545395755dbd341c43d1b8c
gravitykloud-tech <gustav.kiewiets@hotmail.com>
2024-11-14 17:28:57 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is demonstartes manual review approvle stage'));
+app.get('/', (req, res) => res.send('This demo manual review approval stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
